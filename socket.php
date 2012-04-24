0fd62e4f646019e89304fad834ff25887505c168
AmiguinhoDesign <amiguinho_design@live.com.pt>
2012-04-24 23:28:48 +0100

    socket creation to connect, Emanuel Amiguinho e Paulo

---
+++ socket.php
@@ -0,0 +1,17 @@
+<?php
+ 
+$string =  $_GET['teste'];
+/* Open a socket to port 1234 on localhost */
+$socket = stream_socket_client('tcp://code.dei.estt.ipt.pt:8080');
+
+/* Send ordinary data via ordinary channels. */
+//fwrite($socket, "Normal data transmit.");
+//$teste = "{ type: 100, teste: 'ola', array:['roleta',20]} ";
+
+/* Send more data out of band. */
+stream_socket_sendto($socket, $string);
+//stream_socket_sendto($socket, $teste, STREAM_OOB);
+
+/* Close it up */
+fclose($socket);
+?>
