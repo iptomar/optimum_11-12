125373250ad873299d6a1737d39b94b65ca39c44
Ricardo Martins <rmartyn@live.com.pt>
2012-05-13 22:01:20 +0100

    home 	Reformulação completa do design 	inserção de login por redes socias e adaptação das variaveis vindas ao header         Registo de novo utilizador, atraves d login efectuado cm facebook login 	colocação de tweets system 	divulgação da pagina em redes socias (facebook and Google +)
    Ricardo Martins & Emanuel amiguinho

---
+++ assets/js/functions.js
@@ -0,0 +1,31 @@
+/* 
+ * all functions
+ * 
+ */
+
+//função que verifica se o mail é validado
+function isValidEmailAddress(emailAddress) {
+    var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
+    return pattern.test(emailAddress);
+};
+                
+//função que retorna a pagina principal
+function goOut(){
+    window.location="/"
+};
+                
+//função que retorna a pagina principal
+function statusNodeJS(){
+    //função que verifica o estado do servers de 3 em 3 segundos
+    $(function() {
+        var intervalID = setInterval(function() {
+            $.ajax({
+                type: 'GET',
+                           url: "socket.php?status"
+            });
+        },3000);
+        setTimeout(function() {
+            clearInterval(intervalID);
+        }, 18000);
+    });
+};
